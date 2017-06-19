291a771755035a20082fc251efe34b63cc153440
songrongbin <srb.123@163.com>
2017-06-19 11:44:57 +0800

    提交新添加文件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,73 @@
 <template>
   <div id="app">
+    <transition name="fade"
+                mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        components: {
+        }
+    }
+
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0px;
+    padding: 0px;
+    /*background: url(assets/bg1.jpg) center !important;
+        background-size: cover;*/
+    // background: #1F2D3D;
+    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  #app {
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    width: 100%;
+  }
+
+  .el-submenu [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+  }
+
+  .el-menu-item [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+  }
+
+  .toolbar {
+    background: #f2f2f2;
+    padding: 10px;
+    //border:1px solid #dfe6ec;
+    margin: 10px 0px;
+    .el-form-item {
+      margin-bottom: 10px;
+    }
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all .2s ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0;
+  }
+</style>
+
+<!--<template>
+  <div id="app">
     <img src="./assets/logo.png">
     <h1></h1>
     <h2>Essential Links</h2>
@@ -16,17 +84,31 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+
+    <firstcomponent></firstcomponent>
+    <secondcomponent></secondcomponent>
+
+    <ul>
+      <li>
+        <router-link to="/first">跳转到第一张页面</router-link>
+        <router-link to="/second">跳转到第二张页面</router-link>
+      </li>
+    </ul>
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
+    import firstcomponent from './component/firstcomponent.vue'
+    import secondcomponent from './component/secondcomponent.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+    components: { firstcomponent, secondcomponent }
 }
 </script>
 
@@ -57,4 +139,7 @@ li {
 a {
   color: #42b983;
 }
-</style>
+</style>-->
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,96 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+//import store from './vuex/store'
+//import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+//import Mock from './mock'
+//Mock.bootstrap();
+//import 'font-awesome/css/font-awesome.min.css'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+//Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+    routes
 })
+
+router.beforeEach((to, from, next) => {
+    //NProgress.start();
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({ path: '/login' })
+    } else {
+        next()
+    }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+const app = new Vue({
+        router: router,
+        render: h => h(App)
+    }).$mount('#app')
+
+/*const app = new Vue({
+    //el: '#app',
+    //template: '<App/>',
+    router,
+    store,
+    //components: { App }
+    render: h => h(App)
+}).$mount('#app')*/
+
+/*import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from "vue-router";
+import VueResource from 'vue-resource'
+import Element from 'element-ui'
+//import 'element-ui/lib/theme-default/index.css'
+
+//开启debug模式
+Vue.config.debug = true;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(Element)
+
+// 定义组件, 也可以像教程之前教的方法从别的文件引入
+const First = { template: '<div><h2>我是第 1 个子页面</h2></div>' }
+import secondcomponent from './component/secondcomponent.vue'
+
+// 创建一个路由器实例
+// 并且配置路由规则
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {
+            path: '/first',
+            component: First
+        },
+        {
+            path: '/second',
+            component: secondcomponent
+        }
+    ]
+})
+
+// 现在我们可以启动应用了！
+// 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
+const app = new Vue({
+    router: router,
+    render: h => h(App)
+}).$mount('#app')*/
